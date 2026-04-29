c3e11046d67d70474c5ca2512c13eefd30254f12
Aditya Patel <adityakdpatel@gmail.com>
2026-04-29 16:24:05 -0400

    start from scratch

---
+++ flake.nix
@@ -0,0 +1,18 @@
+  {
+    inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    outputs = { self, nixpkgs }: let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.llama-cpp
+          pkgs.cmake
+          pkgs.ninja
+          pkgs.yaml-cpp
+          pkgs.pkg-config
+        ];
+      };
+    };
+  }
